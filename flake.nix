a3d754f1e48ac5690926c5a72c447727bd380c92
Justin Speegle <justin@speegs.com>
2023-02-07 12:56:49 -0800

    flake it

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Scripts for personal use";
+
+  inputs =  {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs } @ inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = inputs.nixpkgs.legacyPackages.${system};
+    tmux-sessionizer = pkgs.writeShellApplication {
+      name = "tmux-sessionizer";
+      runtimeInputs = with pkgs; [ tmux ];
+      text = builtins.readFile ./tmux-sessionizer.sh;
+    };
+  in {
+    packages.${system}.tmux-sessionizer = tmux-sessionizer;
+  };
+}
