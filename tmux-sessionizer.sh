88c8e6e31447f98eb8dfddeefd4208c14f9100f5
Justin Speegle <justin@speegs.com>
2023-02-07 19:32:45 -0800

    change order of find options to address warning

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-path=${1:-$(find ~ /etc/nixos{,/packages{,/scripts}} -type d \
-    -mindepth 1 -maxdepth 1| fzf)}
+path=${1:-$(find ~ /etc/nixos{,/packages{,/scripts}} -mindepth 1 -maxdepth 1 \
+    -type d | fzf)}
 name=$(basename "$path" | tr . _)
 # pid=$(pgrep tmux)
 cmd=${2:-bash -l}
